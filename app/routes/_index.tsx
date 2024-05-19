dd6960fc0fa848804cf768df72adc57a3974458c
Try Ajitiono <ballinst@gmail.com>
2024-05-19 13:36:22 +0000

    seems like a way to test it is to test both Asia/Taipei and Asia/Jakarta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,10 @@
+import dayjs from "dayjs";
 import type { MetaFunction } from "@remix-run/node";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +14,13 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const date = dayjs("2024-05-19T12:38:03.440Z");
+  const formatted = date.tz("Asia/Taipei").format();
+  console.info(formatted, date.tz());
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <p>{formatted}</p>
     </div>
   );
 }
--- scripts/mock-date.ts
+++ scripts/mock-date.ts
@@ -1,30 +1,37 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
-const TIMEZONE_OFFSET = -120;
-const ADDED_HOURS = Math.floor(TIMEZONE_OFFSET / 60) * -1;
-const ADDED_MINUTES = Math.floor(TIMEZONE_OFFSET % 60) * -1;
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
-Object.assign(Date.prototype, {
-  getTimezoneOffset() {
-    return TIMEZONE_OFFSET;
-  },
-  getHours_: Date.prototype.getHours,
-  getHours() {
-    return this.getHours_() + ADDED_HOURS;
-  },
-  getMinutes_: Date.prototype.getMinutes,
-  getMinutes() {
-    return this.getMinutes_() + ADDED_MINUTES;
-  },
-});
+// const TIMEZONE_OFFSET = -120;
+// const ADDED_HOURS = Math.floor(TIMEZONE_OFFSET / 60) * -1;
+// const ADDED_MINUTES = Math.floor(TIMEZONE_OFFSET % 60) * -1;
 
-const date = new Date();
-console.info(date.getTimezoneOffset());
-console.info(date.toLocaleDateString());
-console.info(date.toLocaleTimeString());
+// Object.assign(Date.prototype, {
+//   getTimezoneOffset() {
+//     return TIMEZONE_OFFSET;
+//   },
+//   getHours_: Date.prototype.getHours,
+//   getHours() {
+//     return this.getHours_() + ADDED_HOURS;
+//   },
+//   getMinutes_: Date.prototype.getMinutes,
+//   getMinutes() {
+//     return this.getMinutes_() + ADDED_MINUTES;
+//   },
+// });
 
-let dayjsDate = dayjs(date);
-console.info(dayjsDate.format());
+// const date = new Date();
+// console.info(date.getTimezoneOffset());
+// console.info(date.toLocaleDateString());
+// console.info(date.toLocaleTimeString());
 
-dayjsDate = dayjsDate.add(3, "hours");
-console.info(dayjsDate.format());
+// let dayjsDate = dayjs(date);
+// console.info(dayjsDate.format());
+
+// dayjsDate = dayjsDate.add(3, "hours");
+// console.info(dayjsDate.format());
+console.info(dayjs().tz("Asia/Taipei"));
+console.info(dayjs.tz.guess());
